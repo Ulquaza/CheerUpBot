e92807c1464f725f61f3c787e0261e0be721e9ca
Ulquaza <mrexperio@mail.ru>
2024-03-06 23:23:38 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace CheerUpBot
+{
+    internal class Program
+    {
+        static ITelegramBotClient bot = new TelegramBotClient("7089171767:AAHdVQzmYFoVWirRI_3TaSKuvUxzInz39Gk");
+
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type == UpdateType.Message)
+            {
+                if (update.Message is null) return;
+                if (update.Message.Text is null) return;
+
+                var message = update.Message;
+                var messageText = message.Text;
+                var chatId = message.Chat.Id;
+                Console.WriteLine($"Received a '{messageText}' message from {message.Chat.Username} in chat {chatId}");
+            }
+        }
+
+        public static Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Запущен бот {bot.GetMeAsync().Result.FirstName}");
+
+            var cts = new CancellationTokenSource();
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = Array.Empty<UpdateType>()
+            };
+
+            bot.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                pollingErrorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token);
+            Console.ReadLine();
+        }
+    }
+}
